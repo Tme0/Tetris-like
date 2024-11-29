9d6e674e34ff507e7c6a4e1dc221d3539bb75f6e
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-11-29 19:23:02 +0100

    Ajout de fonctions d'affichages plus jolies

---
+++ headers/graphics.h
@@ -0,0 +1,26 @@
+/* Ce fichier gère l'affichage des éléments du jeu à l'écran (plateau, pièces et menus).*/
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include "../headers/input.h"
+#include "../headers/game.h"
+#include "../headers/piece.h"
+#include "../headers/plateau.h"
+#define N 50
+
+void initialiserFenetre();
+void afficherMenu();
+void afficherParametres();
+void afficherPlateau(plateau monPlateau);
+void afficherPiece(piece maPiece);
+void afficherScores(scores mesScores);
+void afficherLogo();
+void afficherContoursEtTextes();
+int afficherScoresPendantPartie();
+void afficherSauvegarde();
+void afficherParametresControles();
+void afficherParametresVideo();
+void afficherParametresAudio();
+
+#endif /* GRAPHICS_H */

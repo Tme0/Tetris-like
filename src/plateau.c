2cb79743d59b35f2be34392175bb14e894ecdff6
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-11-29 19:37:14 +0100

    Suppression de break (la prof aime pas) et ajout de fonctions

---
+++ src/plateau.c
@@ -0,0 +1,82 @@
+/*Ce fichier gère la création et le mouvement des pièces.*/
+
+#include "../headers/plateau.h"
+
+plateau initialiserPlateau(plateau monPlateau){
+    int i, j;
+    for (i=0 ; i<20 ; i++) {
+        for (j=0 ; j<10 ; j++) {
+            monPlateau.state[i][j] = 0;
+            monPlateau.couleur[i][j] = MLV_COLOR_BLACK;
+        }
+    }
+    monPlateau.x = 0;
+    monPlateau.y = 0;
+    monPlateau.largeur = 10;
+    monPlateau.hauteur = 20;
+    return monPlateau;
+}
+
+void majPlateau(piece maPiece, plateau *monPlateau) {
+    int i, j;
+    for (i = 0 ; i < TAILLE_PIECE ; i++) {
+        for (j = 0 ; j < TAILLE_PIECE ; j++) {
+            if (maPiece.idPiece.forme[i][j] == 1) {
+                monPlateau->state[maPiece.y + i][maPiece.x + j] = 1;
+                monPlateau->couleur[maPiece.y + i][maPiece.x + j] = maPiece.idPiece.couleur;
+            }
+        }
+    }
+}
+
+void majApresChute(piece maPiece, plateau *monPlateau) { /* Permet d'effacer la pièce à la frame précédente (actualisation) */
+    int i, j;
+    for (i = 0 ; i < TAILLE_PIECE ; i++) {
+        for (j = 0 ; j < TAILLE_PIECE ; j++) {
+            if (maPiece.idPiece.forme[i][j] == 1) {
+                monPlateau->state[maPiece.y + i][maPiece.x + j] = 0;
+                monPlateau->couleur[maPiece.y + i][maPiece.x + j] = MLV_COLOR_BLACK;
+            }
+        }
+    }
+}
+
+int ligneComplete(plateau monPlateau) {
+    int i, j, complet;
+    i = 0;
+    complet = 0;
+    while (complet != 10 && i < 20){
+        complet = 0;
+        for (j = 0 ; j < 10 ; j++){
+            if (monPlateau.state[i][j] == 1){
+                complet++;
+            }
+        }
+        i++;
+    }
+    if (complet == 10) {
+        printf("Ligne %d complète\n", i);
+        return i-1;
+    }
+    return -1;
+}
+
+void supprimerLigne(plateau *monPlateau, int ligne) {
+    int j;
+    for (j = 0 ; j < 10 ; j++) {
+        monPlateau->state[ligne][j] = 0;
+        monPlateau->couleur[ligne][j] = MLV_COLOR_BLACK;
+    }
+    descendreLignes(monPlateau, ligne);
+    printf("Ligne %d supprimée\n", ligne);
+}
+
+void descendreLignes(plateau *monPlateau, int ligne) {
+    int i, j;
+    for (i = ligne ; i > 0 ; i--) {
+        for (j = 0 ; j < 10 ; j++) {
+            monPlateau->state[i][j] = monPlateau->state[i-1][j];
+            monPlateau->couleur[i][j] = monPlateau->couleur[i-1][j];
+        }
+    }
+}

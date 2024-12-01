7c33552b2cc43bbae8f8e847a7dd3d887e5e03b3
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-01 19:49:00 +0100

    Ajout de fonctions pour tourner les pièces

--- src/piece.c
+++ src/piece.c
@@ -30,7 +30,7 @@ piece creerPiece(int id) {
         case 2:
             for (i = 0; i < TAILLE_PIECE; i++) {
                 for (j = 0; j < TAILLE_PIECE; j++) {
-                    if (i == 0) {
+                    if (i == 1) {
                         maPiece.idPiece.forme[i][j] = 1;
                     } 
                     else {
@@ -41,7 +41,7 @@ piece creerPiece(int id) {
             maPiece.idPiece.couleur = MLV_COLOR_TURQUOISE2;
             break;
 
-        /* L inversé */
+        /* J */
         case 3:
             for (i = 0; i < TAILLE_PIECE; i++) {
                 for (j = 0; j < TAILLE_PIECE; j++) {
@@ -56,7 +56,7 @@ piece creerPiece(int id) {
             maPiece.idPiece.couleur = MLV_COLOR_BLUE;
             break;
 
-        /* bloc */
+        /* O */
         case 4:
             for (i = 0; i < TAILLE_PIECE; i++) {
                 for (j = 0; j < TAILLE_PIECE; j++) {
@@ -119,104 +119,77 @@ piece creerPiece(int id) {
     return maPiece;
 }
 
-/* J'allais le faire mais jsp comment le faire */
-/* piece tournerPiece(int id){ */
-/*     int i; */
-/*     piece maPiece; */
-/*     maPiece.idPiece.id = id; */
-/*     maPiece.orientation = 1; */
-/*     maPiece.vitesse = vit; */
-/*     switch (id) { */
-/*         /\* L *\/ */
-/*         case 1: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 2 || i == 4 || i == 5 || i == 6) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_ORANGERED; */
-/*             break; */
+void tournerPieceDroite(piece *maPiece) {
+  int i, j;
+  int tmp[3][3];
+  
+  if (maPiece->idPiece.id != 2 && maPiece->idPiece.id != 4){
+    for (i = 0; i < 3; i++){
+      for (j = 0 ; j < 3; j++){
+        tmp[i][j] = maPiece->idPiece.forme[j][2-i];
+      }
+    }
+    for (i = 0; i < 3; i++){
+      for (j = 0 ; j < 3; j++){
+	maPiece->idPiece.forme[i][j] = tmp[i][j];
+      }
+    }
+  }
+  
+  if (maPiece->idPiece.id == 2){
+    switch (maPiece->orientation) {
+      case 0 :
+	for (i = 0; i < TAILLE_PIECE; i++) {
+	  for (j = 0; j < TAILLE_PIECE; j++) {
+	    if (j == 2) {
+	      maPiece->idPiece.forme[i][j] = 1;
+	    } 
+	    else {
+	      maPiece->idPiece.forme[i][j] = 0;
+	    }
+	  }
+	}
+	break;
+	
+      case 1 :
+	for (i = 0; i < TAILLE_PIECE; i++) {
+	  for (j = 0; j < TAILLE_PIECE; j++) {
+	    if (i == 2) {
+	      maPiece->idPiece.forme[i][j] = 1;
+	    } 
+	    else {
+	      maPiece->idPiece.forme[i][j] = 0;
+	    }
+	  }
+	}
+	break;
+	
+      case 2 :
+	for (i = 0; i < TAILLE_PIECE; i++) {
+	  for (j = 0; j < TAILLE_PIECE; j++) {
+	    if (j == 1) {
+	      maPiece->idPiece.forme[i][j] = 1;
+	    } 
+	    else {
+	      maPiece->idPiece.forme[i][j] = 0;
+	    }
+	  }
+	}
+	break;
 
-/*         /\* | *\/ */
-/*         case 2: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 0 || i == 1 || i == 2 || i == 3) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_TURQUOISE2; */
-/*             break; */
-
-/*         /\* L inversé *\/ */
-/*         case 3: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 0 || i == 4 || i == 5 || i == 6) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_BLUE; */
-/*             break; */
-
-/*         /\* bloc *\/ */
-/*         case 4: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 0 || i == 1 || i == 4 || i == 5) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_GOLD; */
-/*             break; */
-
-/*         /\* Z *\/ */
-/*         case 5: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 0 || i == 1 || i == 5 || i == 6) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_rgba(220,20,60,MLV_ALPHA_OPAQUE); */
-/*             break; */
-
-/*         /\* S *\/  */
-/*         case 6: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 1 || i == 2 || i == 4 || i == 5) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_GREEN; */
-/*             break; */
-
-/*         /\* T *\/ */
-/*         case 7: */
-/*             for (i = 0; i < TAILLE_PIECE; i++) { */
-/*                 if (i == 0 || i == 1 || i == 2 || i == 5) { */
-/*                     maPiece.idPiece.forme[i] = 1; */
-/*                 }  */
-/*                 else { */
-/*                     maPiece.idPiece.forme[i] = 0; */
-/*                 } */
-/*             } */
-/*             maPiece.idPiece.couleur = MLV_COLOR_PURPLE2; */
-/*             break; */
-/*     }    */
-/*     return maPiece; */
-/* } */
+      case 3 :
+	for (i = 0; i < TAILLE_PIECE; i++) {
+	  for (j = 0; j < TAILLE_PIECE; j++) {
+	    if (i == 1) {
+	      maPiece->idPiece.forme[i][j] = 1;
+	    } 
+	    else {
+	      maPiece->idPiece.forme[i][j] = 0;
+	    }
+	  }
+	}
+	break;
+    }
+  }
+  maPiece->orientation = (maPiece->orientation + 1) % 4;
+}

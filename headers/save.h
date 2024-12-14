1ec48be2bb31174a6df65523dea00d0e1c8e393c
DamienGrjn <gd05295y@lsc4vs16a78s-1>
2024-12-14 16:02:12 +0100

    Début d'ajouts de menus

--- headers/save.h
+++ headers/save.h
@@ -3,6 +3,13 @@
 #ifndef SAVE_H
 #define SAVE_H
 
+#include <MLV/MLV_all.h>
+#include "../headers/piece.h"
+#include "../headers/plateau.h"
+
+
 void sauvegarderScore(int score);
+int sauvegarderSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
+int chargerSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
 
 #endif /* SAVE_H */

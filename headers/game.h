2b1feebc59f9e4497f4833b9d37d5ec19f70edc1
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-15 19:53:28 +0100

    Ajout de menus et des saves, pas fini

--- headers/game.h
+++ headers/game.h
@@ -6,18 +6,13 @@
 #include "../headers/piece.h"
 #include "../headers/plateau.h"
 
-typedef struct scores {
-    int score[10];
-} scores;
-
-void menu(int menuActif);
+int menu(int menuActif);
 int jouer();
 int estPosee(piece maPiece, plateau monPlateau);
 int colisionDroite(piece maPiece, plateau monPlateau);
 int colisionGauche(piece maPiece, plateau monPlateau);
 void tomberPiece(piece *maPiece, plateau *monPlateau);
 int finJeu(piece maPiece);
-scores recupererScores();
 
 
 #endif /* GAME_H */

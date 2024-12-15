44992aa3a9dc181a88c01dc09b02f76b5ce8fd26
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-15 19:54:04 +0100

    Ajout de menus et des saves, pas fini

--- src/save.c
+++ src/save.c
@@ -20,8 +20,20 @@ void sauvegarderScore(int score) {
 }
 
 int sauvegarderSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop) {
-    int i;
+    int i, j;
     FILE *fichier;
+    printf("continuer : %d\n", *continuer);
+    printf("niveau : %d\n", *niveau);
+    printf("score : %d\n", *score);
+    printf("reserveUtilisee : %d\n", *reserveUtilisee);
+    printf("hardDrop : %d\n", *hardDrop);
+
+    for (i=0;i<20;i++){
+      for (j=0;j<10;j++){
+	printf("%d ", monPlateau->state[i][j]);
+      }
+      printf("\n");
+    }
     
     if ((fichier = fopen(nom, "wb+")) == NULL) {
         printf("Erreur\n");
@@ -32,8 +44,8 @@ int sauvegarderSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouv
 
     /* Save de la grille */
     for (i = 0 ; i < monPlateau->hauteur ; i++) {
-        if (fwrite(monPlateau->state[i], sizeof(int), (size_t)monPlateau->hauteur, fichier) != (size_t)monPlateau->hauteur) {
-            fprintf(stderr, "Erreur lors de l'écriture de la grille.\n");
+        if (fwrite(monPlateau->state[i], sizeof(int), (size_t)monPlateau->largeur, fichier) != (size_t)monPlateau->largeur) {
+	    fprintf(stderr, "Erreur : écriture matrice à la ligne %d \n", i);
             fclose(fichier);
             return -1;
         }
@@ -41,7 +53,7 @@ int sauvegarderSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouv
 
     /* Save des couleurs de la grille */
     for (i = 0 ; i < monPlateau->hauteur ; i++) {
-        if (fwrite(monPlateau->couleur[i], sizeof(MLV_Color), (size_t)monPlateau->hauteur, fichier) != (size_t)monPlateau->hauteur) {
+        if (fwrite(monPlateau->couleur[i], sizeof(MLV_Color), (size_t)monPlateau->largeur, fichier) != (size_t)monPlateau->largeur) {
             fprintf(stderr, "Erreur lors de l'écriture des couleurs du plateau.\n");
             fclose(fichier);
             return -1;
@@ -170,6 +182,7 @@ int sauvegarderSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouv
         fclose(fichier);
         return -1;
     }
+    fclose(fichier);
     return 1;
 }
 
@@ -183,147 +196,149 @@ int chargerSave(char nom[10], plateau *monPlateau, piece *maPiece, int *mouvemen
         return -1;
     }
 
-    /* ---------- SAVE DU PLATEAU ---------- */
+    /* ---------- CHARGE DU PLATEAU ---------- */
 
-    /* Save de la grille */
+    /* Charge de la grille */
     for (i = 0 ; i < monPlateau->hauteur ; i++) {
-        if (fread(monPlateau->state[i], sizeof(int), (size_t)monPlateau->hauteur, fichier) != (size_t)monPlateau->hauteur) {
-            fprintf(stderr, "Erreur lors de l'écriture de la grille.\n");
+        if (fread(monPlateau->state[i], sizeof(int), (size_t)monPlateau->largeur, fichier) != (size_t)monPlateau->largeur) {
+	  /*fprintf(stderr, "Erreur lors de l'écriture de la grille.\n");*/
+	    fprintf(stderr, "Erreur : lecture matrice à la ligne %d \n", i);
             fclose(fichier);
             return -1;
         }
     }
 
-    /* Save des couleurs de la grille */
+    /* Charge des couleurs de la grille */
     for (i = 0 ; i < monPlateau->hauteur ; i++) {
-        if (fread(&monPlateau->couleur[i], sizeof(MLV_Color), (size_t)monPlateau->hauteur, fichier) != (size_t)monPlateau->hauteur) {
-            fprintf(stderr, "Erreur lors de l'écriture des couleurs du plateau.\n");
+        if (fread(&monPlateau->couleur[i], sizeof(MLV_Color), (size_t)monPlateau->largeur, fichier) != (size_t)monPlateau->largeur) {
+            fprintf(stderr, "Erreur lors de la lecture des couleurs du plateau.\n");
             fclose(fichier);
             return -1;
         }
     }
 
-    /* Save de reserveOccupee */
+    /* Charge de reserveOccupee */
     if (fread(&monPlateau->reserveOccupee, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de reserveOccupee.\n");
+        fprintf(stderr, "Erreur lors de la lecture de reserveOccupee.\n");
         fclose(fichier);
         return -1;
     }
 
-    /* Save de la réserve, si elle est occupée */
+    /* Charge de la réserve, si elle est occupée */
     if (monPlateau->reserveOccupee == 1){
         if (fread(&monPlateau->pieceReserve, sizeof(piece), 1, fichier) != 1) {
-            fprintf(stderr, "Erreur lors de l'écriture de la réserve.\n");
+            fprintf(stderr, "Erreur lors de la lecture de la réserve.\n");
             fclose(fichier);
             return -1;
         }
     }
 
-    /* Save du tableau des pièces suivantes */
+    /* Charge du tableau des pièces suivantes */
     for (i = 0 ; i < 5 ; i++) {
         if (fread(&monPlateau->piecesSuivantes[i], sizeof(piece), 1, fichier) != 1) {
-            fprintf(stderr, "Erreur lors de l'écriture des pièces suivantes.\n");
+            fprintf(stderr, "Erreur lors de la lecture des pièces suivantes.\n");
             fclose(fichier);
             return -1;
         }
     }
     
 
-    /* ---------- SAVE DE LA PIECE ACTIVE ---------- */
+    /* ---------- CHARGE DE LA PIECE ACTIVE ---------- */
 
-    /* Save de l'id de la pièce */
+    /* Charge de l'id de la pièce */
     if (fread(&maPiece->idPiece.id, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de l'id de la pièce.\n");
+        fprintf(stderr, "Erreur lors de la lecture de l'id de la pièce.\n");
         fclose(fichier);
         return -1;
     }
 
-    /* Save de la couleur de la pièce */
+    /* Charge de la couleur de la pièce */
     if (fread(&maPiece->idPiece.couleur, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de la couleur de la pièce.\n");
+        fprintf(stderr, "Erreur lors de la lecture de la couleur de la pièce.\n");
         fclose(fichier);
         return -1;
     }
 
-    /* Save de la forme de la pièce */
+    /* Charge de la forme de la pièce */
     for (i = 0 ; i < 4 ; i++) {
         if (fread(maPiece->idPiece.forme[i], sizeof(int), 4, fichier) != 4) {
-            fprintf(stderr, "Erreur lors de l'écriture de la forme de la pièce.\n");
+            fprintf(stderr, "Erreur lors de la lecture de la forme de la pièce.\n");
             fclose(fichier);
             return -1;
         }
     }
 
-    /* Save du x de la pièce */
+    /* Charge du x de la pièce */
     if (fread(&maPiece->x, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture du x de la pièce.\n");
+        fprintf(stderr, "Erreur lors de la lecture du x de la pièce.\n");
         fclose(fichier);
         return -1;
     }
 
-    /* Save du y de la pièce */
+    /* Charge du y de la pièce */
     if (fread(&maPiece->y, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture du y de la pièce.\n");
+        fprintf(stderr, "Erreur lors de la lecture du y de la pièce.\n");
         fclose(fichier);
         return -1;
     }
 
-    /* Save de l'orientation de la pièce */
+    /* Charge de l'orientation de la pièce */
     if (fread(&maPiece->orientation, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de l'orientation de la pièce.\n");
+        fprintf(stderr, "Erreur lors de la lecture de l'orientation de la pièce.\n");
         fclose(fichier);
         return -1;
     }
 
 
-    /* ---------- SAVE DES DIFFERENTES VARIABLES ---------- */
+    /* ---------- CHARGE DES DIFFERENTES VARIABLES ---------- */
 
     if (fread(&mouvementVertical, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de mouvementVertical.\n");
+        fprintf(stderr, "Erreur lors de la lecture de mouvementVertical.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&frame, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de frame.\n");
+        fprintf(stderr, "Erreur lors de la lecture de frame.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&aBouge, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de aBouge.\n");
+        fprintf(stderr, "Erreur lors de la lecture de aBouge.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&continuer, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de continuer.\n");
+        fprintf(stderr, "Erreur lors de la lecture de continuer.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&niveau, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de niveau.\n");
+        fprintf(stderr, "Erreur lors de la lecture de niveau.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&score, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de score.\n");
+        fprintf(stderr, "Erreur lors de la lecture de score.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&reserveUtilisee, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de reserveUtilisee.\n");
+        fprintf(stderr, "Erreur lors de la lecture de reserveUtilisee.\n");
         fclose(fichier);
         return -1;
     }
 
     if (fread(&hardDrop, sizeof(int), 1, fichier) != 1) {
-        fprintf(stderr, "Erreur lors de l'écriture de hardDrop.\n");
+        fprintf(stderr, "Erreur lors de la lecture de hardDrop.\n");
         fclose(fichier);
         return -1;
     }
+    fclose(fichier);
     return 1;
 }

502954e33a934f4f3652f289579b5e43989d13e7
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-16 22:07:08 +0100

    Amélioration des menus, pas fini

--- headers/input.h
+++ headers/input.h
@@ -7,7 +7,6 @@
 #include "./types.h"
 
 piece resoudreEvenement(MLV_Keyboard_button touche, piece maPiece, plateau monPlateau);
-void mettreEnPause(plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
 int selectionMenuPrincipal(int nbBoutons, bouton boutons[BOUTONS]);
 void selectionMenuPause(int nbBoutons, bouton boutons[BOUTONS], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
 void selectionMenuSauvegarder(int nbBoutons, bouton boutons[BOUTONS], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);

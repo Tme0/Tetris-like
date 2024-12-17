a3ef52a1ee84ee3f0565015c86e69c20ec6a4063
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-17 23:33:52 +0100

    aBouge supprimé

--- headers/save.h
+++ headers/save.h
@@ -6,7 +6,7 @@
 #include "./types.h"
 
 void sauvegarderScore(int score);
-int sauvegarderSave(char nom[18], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
-int chargerSave(char nom[18], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *aBouge, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
+int sauvegarderSave(char nom[23], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
+int chargerSave(char nom[23], plateau *monPlateau, piece *maPiece, int *mouvementVertical, int *frame, int *continuer, int *niveau, int *score, int *reserveUtilisee, int *hardDrop);
 
 #endif /* SAVE_H */

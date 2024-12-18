a226d597126b78662a34d97ea9f5bc999fec31a9
DamienGrjn <damien@LAPTOP-N04F54UM.>
2024-12-18 14:58:12 +0100

    Suppression de majApresChute

--- headers/plateau.h
+++ headers/plateau.h
@@ -9,7 +9,6 @@ plateau initialiserPlateau(plateau monPlateau);
 void majPlateau(piece maPiece, plateau *monPlateau);
 void mettreEnReserve(piece *maPiece, plateau *monPlateau);
 void majPiecesSuivantes(piece *maPiece, plateau *monPlateau);
-void majApresChute(piece maPiece, plateau *monPlateau);
 int ligneComplete(plateau monPlateau);
 void supprimerLigne(plateau *monPlateau, int ligne);
 void descendreLignes(plateau *monPlateau, int ligne);
